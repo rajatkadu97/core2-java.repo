5b5f8f215212989ffc187bd58ab2dad7cf92b318
rajatkadu97 <rajatkadu97@gmail.com>
2026-04-27 18:58:14 +0530

    loop in java

---
+++ src/core/datatype/DataType.java
@@ -0,0 +1,13 @@
+package core.datatype;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 12;
+		int b = 15;
+		System.out.println("print a " + a);
+		System.out.println("print b " + b);
+	}
+
+}
---
+++ src/loops/loops.java
@@ -0,0 +1,18 @@
+package loops;
+
+public class loops {
+
+	public static void main(String[] args) {
+		// 1. initialization
+//		2. codition 
+//		3. incri/decr
+		System.out.println("Start");
+
+		for (int i = 0; i < 5; i++) {
+
+			System.out.println("hii" + i);
+		}
+
+	}
+
+}
